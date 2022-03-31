4fd84ed7b8004c41de0bad9006f776b9b7955307
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2022-03-31 12:14:50 +0200

    Rename index.js and add functionality

---
+++ src/provider.js
@@ -0,0 +1,105 @@
+const Web3 = require('web3')
+const fs = require('fs');
+var Contract = require('web3-eth-contract')
+
+
+const _callbacks = {
+    TourTokenFactory: [],
+    BFactory: [],
+    TourTokenOrder: []
+}
+
+var netId = 1
+
+module.exports.setNetworkId = function (new_netId) {
+    netId = new_netId
+}
+
+module.exports.setProviderURL = function (nodeURL) {
+    Contract.setProvider(nodeURL);
+}
+
+module.exports.initTourTokenFactoryListener = function (TourTokenFactoryMeta) {
+    let truffle_ABI_string = fs.readFileSync(TourTokenFactoryMeta);
+    let truffle_obj = JSON.parse(truffle_ABI_string);
+    let abi = truffle_obj.abi
+    let tourtokenfactoryAddress = truffle_obj.networks[netId].address
+
+    let TourTokenFactory = new Contract(abi, tourtokenfactoryAddress);
+    console.log("[provider-js] Listening to New TourToken created at " + tourtokenfactoryAddress);
+    TourTokenFactory.events.allEvents({}, (err, event) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            if (event.event == "TokenRegistered")
+            {
+                executeCallbacks(_callbacks.TourTokenFactory, event.returnValues)
+            }
+        }
+    })
+}
+
+module.exports.initBFactoryListener = function (BFactoryMeta) {
+    let truffle_ABI_string = fs.readFileSync(BFactoryMeta);
+    let truffle_obj = JSON.parse(truffle_ABI_string);
+    let abi = truffle_obj.abi
+    let bfactoryAddress = truffle_obj.networks[netId].address
+
+    let BFactory = new Contract(abi, bfactoryAddress);
+    console.log("[provider-js] Listening to New BPool created at " + bfactoryAddress);
+    BFactory.events.allEvents({}, (err, event) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            if (event.event == "LOG_NEW_POOL")
+            {
+                executeCallbacks(_callbacks.BFactory, event.returnValues)
+            }
+        }
+    })
+}
+
+module.exports.addTT = function (TTMeta, address) {
+    let truffle_ABI_string = fs.readFileSync(TTMeta);
+    let truffle_obj = JSON.parse(truffle_ABI_string);
+    let abi = truffle_obj.abi
+
+    let TourToken = new Contract(abi, address);
+    console.log("[provider-js] Listening to TourToken at " + address);
+    TourToken.events.allEvents({}, (err, event) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            if (event.event == "OrderStarted")
+            {
+                executeCallbacks(_callbacks.TourTokenOrder, event.returnValues)
+            }
+        }
+    })
+}
+
+
+module.exports.registerCallback = function (type, cb) {
+    switch (type) {
+        case 'TourTokenFactory':
+            _callbacks.TourTokenFactory.push(cb)
+            break;
+        case 'BFactory':
+            _callbacks.BFactory.push(cb)
+            break;
+        case 'TourTokenOrder':
+            _callbacks.TourTokenOrder.push(cb)
+            break;
+        default:
+            throw new Error("Unknown Type")
+    }
+}
+
+function executeCallbacks(callbacks, event) {
+    callbacks.forEach((element) => {
+        element(event)
+    })
+}
