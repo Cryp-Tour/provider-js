e0ef0de01f4607eaf56325bfae22bb38a37cb563
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2022-03-31 12:14:09 +0200

    Add example instead of test

--- test/test.js
+++ test/test.js
@@ -1,24 +1,19 @@
-const provider = require('../src/index');
-const fs = require('fs');
-var assert = require('assert');
+const provider = require('../src/provider');
 
-describe("Provider", () => {
+provider.setNetworkId('5777')
+provider.setProviderURL('ws://localhost:7545')
+provider.initTourTokenFactoryListener('../../contracts/build/contracts/TourTokenFactory.json')
+provider.initBFactoryListener('../../contracts/build/contracts/BFactory.json')
 
-    let truffle_ABI_string = fs.readFileSync('../contracts/build/contracts/TourTokenFactory.json');
-    let truffle_obj = JSON.parse(truffle_ABI_string);
-    let ABI = truffle_obj.abi
-    let address = truffle_obj.networks['5777'].address
-    provider.init('ws://localhost:7545', ABI, address)
-    provider.registerCallback((err, event) => {
-        if (err) {
-            console.log(err)
-        }
-        else {
-            console.log(event)
-        }
-    })
+provider.registerCallback("TourTokenOrder", (event) => {
+    console.log(event)
+})
 
-    it('Provider.Contract should not be undefined', () => {
-        assert.notStrictEqual(undefined, provider.Contract);
-    })
+provider.registerCallback("TourTokenFactory", (event) => {
+    console.log(event)
+    provider.addTT('../../contracts/build/contracts/TourTokenTemplate.json', event.tokenAddress)
+})
+
+provider.registerCallback("BFactory", (event) => {
+    console.log(event)
 })
