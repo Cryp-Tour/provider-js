0cae7fabd38262f965a77573db0174ce567abcc6
Benedict Wetzel <benedict.f.wetzel@gmx.de>
2022-05-03 11:35:33 +0200

    Add Listener for BPools to provider

--- src/provider.js
+++ src/provider.js
@@ -1,12 +1,13 @@
 const Web3 = require('web3')
 const fs = require('fs');
-var Contract = require('web3-eth-contract')
+var Contract = require('web3-eth-contract');
 
 
 const _callbacks = {
     TourTokenFactory: [],
     BFactory: [],
-    TourTokenOrder: []
+    TourTokenOrder: [],
+    BPool: []
 }
 
 var netId = 1
@@ -26,14 +27,13 @@ module.exports.initTourTokenFactoryListener = function (TourTokenFactoryMeta) {
     let tourtokenfactoryAddress = truffle_obj.networks[netId].address
 
     let TourTokenFactory = new Contract(abi, tourtokenfactoryAddress);
-    console.log("[provider-js] Listening to New TourToken created at " + tourtokenfactoryAddress);
+    console.log("[provider-js] Listening to new TourToken created at " + tourtokenfactoryAddress);
     TourTokenFactory.events.allEvents({}, (err, event) => {
         if (err) {
             console.log(err)
         }
         else {
-            if (event.event == "TokenRegistered")
-            {
+            if (event.event == "TokenRegistered") {
                 executeCallbacks(_callbacks.TourTokenFactory, event.returnValues)
             }
         }
@@ -47,14 +47,13 @@ module.exports.initBFactoryListener = function (BFactoryMeta) {
     let bfactoryAddress = truffle_obj.networks[netId].address
 
     let BFactory = new Contract(abi, bfactoryAddress);
-    console.log("[provider-js] Listening to New BPool created at " + bfactoryAddress);
+    console.log("[provider-js] Listening to new BPools created at " + bfactoryAddress);
     BFactory.events.allEvents({}, (err, event) => {
         if (err) {
             console.log(err)
         }
         else {
-            if (event.event == "LOG_NEW_POOL")
-            {
+            if (event.event == "LOG_NEW_POOL") {
                 executeCallbacks(_callbacks.BFactory, event.returnValues)
             }
         }
@@ -73,14 +72,31 @@ module.exports.addTT = function (TTMeta, address) {
             console.log(err)
         }
         else {
-            if (event.event == "OrderStarted")
-            {
+            if (event.event == "OrderStarted") {
                 executeCallbacks(_callbacks.TourTokenOrder, event.returnValues)
             }
         }
     })
 }
 
+module.exports.addBPool = function (BPoolMeta, address) {
+    let truffle_ABI_string = fs.readFileSync(BPoolMeta);
+    let truffle_obj = JSON.parse(truffle_ABI_string);
+    let abi = truffle_obj.abi
+
+    let BPool = new Contract(abi, address);
+    console.log("[provider-js] Listening to BPool at " + address);
+    BPool.events.allEvents({}, (err, event) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            if (event.event == "LOG_INIT") {
+                executeCallbacks(_callbacks.BPool, event.returnValues)
+            }
+        }
+    })
+}
 
 module.exports.registerCallback = function (type, cb) {
     switch (type) {
@@ -92,6 +108,9 @@ module.exports.registerCallback = function (type, cb) {
             break;
         case 'TourTokenOrder':
             _callbacks.TourTokenOrder.push(cb)
+            break;
+        case 'BPool':
+            _callbacks.BPool.push(cb)
             break;
         default:
             throw new Error("Unknown Type")
